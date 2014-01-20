96c84abf1c68b49d336cc1d323b75def13d31c71
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-20 18:28:54 -0500

    Renamed template names to be slightly more descriptive.

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'index.html', {
+    return render(request, 'groupfit_home.html', {
     },
     )
 
--- groupfit/users/views.py
+++ groupfit/users/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'index.html', {
+    return render(request, 'user_home.html', {
     },
     )
