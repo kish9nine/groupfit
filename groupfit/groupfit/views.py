908ffcc0ce47aa0bde15b06d2389a375cfae1e09
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-20 17:51:52 -0500

    Added basic website views.

---
+++ groupfit/groupfit/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, 'index.html', {
+    },
+    )
+
+def about(request):
+    return render(request, 'about.html', {
+    },
+    )
+
+def contact(request):
+    return render(request, 'contact.html', {
+    },
+    )
+
+def terms(request):
+    return render(request, 'terms.html', {
+    },
+    )
