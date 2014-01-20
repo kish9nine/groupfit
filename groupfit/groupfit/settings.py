20af28d81154e31b6e249dfa49a8bf5dd07ca1aa
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-20 17:30:46 -0500

    Updated settings file.

--- groupfit/groupfit/settings.py
+++ groupfit/groupfit/settings.py
@@ -1,33 +1,48 @@
+###############################################################################
+
 """
 Django settings for groupfit project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+General application settings.
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import secret
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!chlc#w+*cfd@_2yjjt3!bql&ab)n_9hquh(9jh7!b2e1w)9!s'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
+SECRET_KEY = secret.SECRET_KEY
 
+ROOT_URLCONF = 'groupfit.urls'
 
-# Application definition
+WSGI_APPLICATION = 'groupfit.wsgi.application'
+
+###############################################################################
+
+"""
+Database settings for connecting to the MySQL database.
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secret.DB_NAME,
+        'USER': secret.DB_USER,
+        'PASSWORD': secret.DB_PASSWORD,
+        'HOST': secret.DB_HOST,
+        'PORT': '',
+    }
+}
+
+###############################################################################
+
+"""
+Django project configurations.
+"""
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -36,6 +51,18 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'groupfit',
+    'south',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,23 +74,27 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'groupfit.urls'
+###############################################################################
 
-WSGI_APPLICATION = 'groupfit.wsgi.application'
+"""
+Authentication settings.
+"""
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+LOGIN_URL = '/login/'
+LOGOUT_URL = '/logout/'
+LOGIN_REDIRECT_URL = '/'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+###############################################################################
+
+"""
+Language / Internationalization settings.
+"""
 
 LANGUAGE_CODE = 'en-us'
 
@@ -75,8 +106,46 @@ USE_L10N = True
 
 USE_TZ = True
 
+###############################################################################
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+"""
+Static file configuration.
+"""
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+###############################################################################
+
+"""
+Simple logging configuration.
+"""
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+###############################################################################
