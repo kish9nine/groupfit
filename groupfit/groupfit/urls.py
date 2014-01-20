bb37b1260fe5459cf9ecf41e62af549974c23e30
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-20 17:51:22 -0500

    Added basic url structure.

--- groupfit/groupfit/urls.py
+++ groupfit/groupfit/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'groupfit.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from groupfit.views import home, about, contact, terms
 
+urlpatterns = patterns('',
+
+    # Website default pages.
+    url(r'^$', home),
+    url(r'^about/$', about),
+    url(r'^contact$', contact),
+    url(r'^terms$', terms),
+
+    # User accounts app pages.
+    url(r'^user/', include('users.urls')),
+
+    # Administrator view.
     url(r'^admin/', include(admin.site.urls)),
+
 )
