fde322b50e5f1d5045d81da6c28e558d7670c9c8
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-21 16:06:12 -0500

    Added group.get_absolute_url

--- groupfit/groups/urls.py
+++ groupfit/groups/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from groups.views import view_group
 
 urlpatterns = patterns('',
+
+    url(r'^(?P<group_pk>\d+)$', view_group),
 
 )
--- groupfit/groups/views.py
+++ groupfit/groups/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from groups.models import WorkoutGroup
 
-# Create your views here.
+def view_group(request, group_pk=-1):
+    group = get_object_or_404( WorkoutGroup, pk=group_pk )
+    return render(request, 'group_profile.html', {
+        'group': group,
+    },
+    )
