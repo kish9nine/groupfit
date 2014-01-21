c219697e3426266549ff29070209582f7fd925a6
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-21 16:06:28 -0500

    Added group profile stub page.

--- groupfit/users/models.py
+++ groupfit/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from groups.models import WorkoutGroup
 from django.db.models.signals import post_save
+from django.core.urlresolvers import reverse
 from tags.models import Tag
 
 
@@ -23,6 +24,13 @@ class UserProfile( models.Model ):
 
     def __unicode__(self):
         return "%s" % self.user.username
+
+    def get_absolute_url(self):
+        return reverse(
+            'users.views.view_user',
+            args=[str(self.pk)]
+        )
+
 
 def create_user_profile( sender, instance, created, **kwargs ):
     if created:
