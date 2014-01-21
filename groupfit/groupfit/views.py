f501c6559b8331fd6ee75bfea76e0f44c63c3503
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-20 22:42:33 -0500

    Updated reference to renamed landing page template.

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render(request, 'groupfit_home.html', {
+    return render(request, 'landing_page.html', {
     },
     )
 
