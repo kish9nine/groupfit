499935d109136c872495b89b860be497b5ea9234
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-21 02:01:58 -0500

    Renamed references to new user view.

--- groupfit/users/urls.py
+++ groupfit/users/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-from users.views import home
+from users.views import view_user
 
 urlpatterns = patterns('',
 
-    # User homepage.
-    url(r'^$', home),
+    # View a user profile.
+    url(r'^(?P<user_pk>\d+)$', view_user),
 
 )
