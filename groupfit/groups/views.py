f5902b58ca0888586b908df9bdba9076a475833d
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-21 19:12:25 -0500

    Renamed group templates

--- groupfit/groups/views.py
+++ groupfit/groups/views.py
@@ -4,13 +4,13 @@ from django.contrib.auth.decorators import login_required
 
 def view_group(request, group_pk=-1):
     group = get_object_or_404( WorkoutGroup, pk=group_pk )
-    return render(request, 'group_profile.html', {
+    return render(request, 'view_group.html', {
         'group': group,
     },
     )
 
 @login_required
 def create_group(request):
-    return render(request, 'group_create.html', {
+    return render(request, 'create_group.html', {
     },
     )
