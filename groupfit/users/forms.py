b3ca1b7a41bb2f8ad0988f47029c78611acb75b9
kish9nine <kish9nine@gmail.com>
2014-01-23 01:18:30 -0500

    Created users/forms.py
    I don't know if model=UserProfile or UserProfile.user

--- groupfit/users/forms.py
+++ groupfit/users/forms.py
@@ -1,9 +1,27 @@
 from django import forms
+from django.forms import ModelForm
+from users.models import UserProfile
 
-class RegisterForm(forms.Form):
-    username = forms.CharField(max_length = 20)
-    password = forms.CharField(max_length = 50)
-    email = forms.EmailField()
-    confirm_email = forms.EmailField()
+
+class RegisterForm(ModelForm):
+    class Meta:
+        #This model form is based on UserProfile.
+        model = UserProfile.user
+        #These are the fields that the user will be inputing. 
+        fields = ['username', 'password', 'confirm_password', 'email']
+        
+        #Labels for each field.
+        label = {
+            'username': 'USERNAME',
+            'password': 'PASSWORD',
+            'confirm_password': 'CONFIRM PASSWORD',
+            'email': 'EMAIL',
+            }
+        
+        
+    #username = forms.CharField(max_length = 20)
+    #password = forms.CharField(max_length = 50)
+    #email = forms.EmailField()
+    #confirm_email = forms.EmailField()
     
     
