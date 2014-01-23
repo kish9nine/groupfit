2a964c6063eb6c58aebfa24f6e18fea248d7ef16
kish9nine <kish9nine@gmail.com>
2014-01-23 03:36:43 -0500

    Update models.py

--- groupfit/users/models.py
+++ groupfit/users/models.py
@@ -51,4 +51,4 @@ creating a UserProfile object along with it.
 def create_user_profile( sender, instance, created, **kwargs ):
     if created:
         UserProfile.objects.create(user=instance)
-post_save.connect(create_user_profile, sender=User)
+#post_save.connect(create_user_profile, sender=User)
