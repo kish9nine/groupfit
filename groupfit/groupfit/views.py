66e3718ea4cd0ec85eec0ab83c1a732913bf212b
kish9nine <kish9nine@gmail.com>
2014-01-23 15:43:32 -0500

    Update views.py

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.mail import send_mail
 from forms import ForgotPasswordForm  #This is where forgot password? form is.
+from django.core.exceptions import ObjectDoesNotExist #I don't need it while I am using User.DoesNotExist instead
 
 def landing_page(request):
     """
@@ -44,11 +45,16 @@ def privacy(request):
 #This page will take in username and email information from the user  
 #And if the username and email belong to the same user, a password reset email will be sent. 
 def forgot(request):
-    """
+    
     #If the request input any information.
     if (request.method == 'POST'):
         forgot_password_form = ForgotPasswordForm(request.POST)
-        username = forgot_password_form.username
-        if username in User.objects.all() and forgot_password_form.pass
-    """
-    return render(request, 'forgot.html', {},)
+        inp_username = forgot_password_form.username
+        inp_email = forgot_password_form.email
+        if User.objects.get(username=inp_username) is not User.DoesNotExist:
+            if User.objects.get(username=inp_username).email == inp_email:
+                send_email('Reset Password', 'Reset Password link', 'admin@groupfit.rouly.net', inp_email)
+                return render(request, 'email_sent.html') #It might be nice if we could turn this into a pop-up instead.
+    else: 
+        forgot_password_form = ForgotPasswordForm(request.POST)
+    return render(request, 'forgot.html', {'forgot_password_form': forgot_password_form},)
