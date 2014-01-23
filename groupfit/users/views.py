2806c4ae70bdbef65ef6c1f22a0555f3c0d11568
kish9nine <kish9nine@gmail.com>
2014-01-23 01:40:45 -0500

    Update views.py

--- groupfit/users/views.py
+++ groupfit/users/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from users.models import UserProfile
+from users.models import UserProfile, create_user_profile
 
 def create_user(request):
     #If the user is trying to add info to the server,
     if request.method == 'POST':
         #Feed as arguments to RegisterForm the inputs from the user.
         create_user_form = RegisterForm(request.POST)
-        #If username and password are of the right length and email is of the valid form, 
+        #If username and password are of the right length and email is of the valid form,
+        #And password and confirm password identical, 
         if create_user_form.is_valid():
             #Don't save the user in creation as a new user yet. 
             new_user = create_user_form.save(commit=False)
             
-            #Then save. 
-            new_user.save()
+            #Then create UserProfile object from 
+            new_userProfile = create_user_profile(new_user)
             
             #Render a Welcome to GroupFit page if the input info is valid. 
+            return redirect('users.views.welcome', new_user.username)
+            
             #Send an email as well. 
             
             
