84e146aa8cfaeecdd76e65105ae6c0e721baa429
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-23 11:34:25 -0500

    Added delete playlist url

--- groupfit/playlists/urls.py
+++ groupfit/playlists/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from playlists.views import create_playlist, view_playlist
+from playlists.views import create_playlist, view_playlist, delete_playlist
 
 urlpatterns = patterns('',
 
     url(r'^create/$', create_playlist),
+    url(r'^delete/(?P<playlist_pk>\d+)$', delete_playlist),
     url(r'^(?P<playlist_pk>\d+)$', view_playlist),
 
 )
