7dfc56d45deb8ac9cafedae52f5e507422125d70
kish9nine <kish9nine@gmail.com>
2014-01-23 02:27:32 -0500

    Update urls.py

--- groupfit/users/urls.py
+++ groupfit/users/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from users.views import view_user
 
 urlpatterns = patterns('',
-
+    # Register
+    url(r'^register', register),
+    
     # Create a user profile.
-    url(r'^create/$', create_user)
+    url(r'^create/$', create_user),
 
     # View a user profile.
     url(r'^(?P<user_pk>\d+)$', view_user),
