111e501d47c16ef65159b47dd190040c1325272b
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-22 23:28:52 -0500

    Added documetnation

--- groupfit/playlists/forms.py
+++ groupfit/playlists/forms.py
@@ -2,15 +2,21 @@ from django import forms
 from django.forms import ModelForm, TextInput
 from playlists.models import Playlist, Track
 
+# This form is a "ModelForm" meaning it fills out an existing Model
 class PlaylistForm( ModelForm ):
     class Meta:
+        # The model this form refers to
         model = Playlist
+        # The fields of the model we're going to use
         fields = ['name', 'tracks']
+        # And fields we don't want to use
         exclude = []
+        # Lables for each field, if you want any.
         labels = {
             'name': 'Name',
             'tracks': 'Tracks',
         }
+        # A Widget is an HTML object. Check the documentation for more.
         widgets = {
             'name': TextInput(attrs={
                 'class': 'form-control',
