d14c7a2ed4764e27c77ab446f7b84cb6301038f8
kish9nine <kish9nine@gmail.com>
2014-01-23 00:25:14 -0500

    Update forms.py

--- groupfit/users/forms.py
+++ groupfit/users/forms.py
@@ -1,1 +1,9 @@
 from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length = 20)
+    password = forms.CharField(max_length = 50)
+    email = forms.EmailField()
+    confirm_email = forms.EmailField()
+    
+    
