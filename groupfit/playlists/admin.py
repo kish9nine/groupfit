8fd016f158e273ad9b65a0e35e2d9e1c2c4cfde3
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-23 00:05:23 -0500

    Register Track with admin

--- groupfit/playlists/admin.py
+++ groupfit/playlists/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from playlists.models import Playlist
+from playlists.models import Playlist, Track
 
 admin.site.register( Playlist )
+admin.site.register( Track )
