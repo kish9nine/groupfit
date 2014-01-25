eb4dfdbad5b430e72a2826ffa3464a1a16e55401
jrouly <michel.rouly@gmail.com>
2014-01-24 21:34:12 -0500

    Added errant comma

--- groupfit/groupfit/settings.py
+++ groupfit/groupfit/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.core.mail'
+    'django.core.mail',
     'groupfit',
     'users',
     'groups',
