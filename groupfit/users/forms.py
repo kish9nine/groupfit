a6730001b42e3c4651c3df12e29ebf55d7145f46
kish9nine <kish9nine@gmail.com>
2014-01-24 20:49:45 -0500

    Update forms.py

--- groupfit/users/forms.py
+++ groupfit/users/forms.py
@@ -3,46 +3,15 @@ from django.forms import ModelForm, TextInput, PasswordInput, EmailInput
 from users.models import UserProfile
 from django.contrib.auth.models import User
 
-
 class RegisterForm(ModelForm):
-    username = forms.CharField(
-        required = True, 
-        #label="USERNAME", 
-        #TextInput(attrs={'class':'form-control', 'placeholer':'Username'})
-        )
-        
-    first_name = forms.CharField(
-        required = True, 
-        #label="FIRSTNAME", 
-        #TextInput(attrs={'class':'form-control', 'placeholer': 'Firstname'})
-        )
-        
-    last_name = forms.CharField(
-        required = True, 
-        #label="LASTNAME", 
-        #TextInput(attrs={'class':'form-control', 'placeholer':'Lastname'})
-        )
-        
-    password = forms.CharField(
-        required = True, 
-        #label="PASSWORD", 
-        #PasswordInput(attrs={'class':'form-control', 'placeholer':'Password'})
-        )
-        
-    """confirm_password = models.CharField(
-        required = True, 
-        label="CONFIRM_PASSWORD", 
-        PasswordInput(attrs={'class':'form-control', 'placeholer':'Confirm password'}))"""
-        
-    email = forms.EmailField(
-        required = True, 
-        #label="EMAIL", 
-        #EmailInput(attrs={'class':'form-control', 'placeholer':'Email'})
-        )
-    
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'password', 'email']
+        
+        #Required fields. Let's see if this works. 
+        required = ['username', 'first_name', 'last_name', 'password', 'email']
+        
         
         #Labels for each field.
         label = {
