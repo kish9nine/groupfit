e71a869f6f14ec4b9d034d96afb1535dee9174a4
kish9nine <kish9nine@gmail.com>
2014-01-24 21:07:39 -0500

    Update views.py

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from forms import ForgotPasswordForm
 from django.core.exceptions import ObjectDoesNotExist
