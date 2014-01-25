fb45ff08c5841545611bd10c43c58626257c6aeb
kish9nine <kish9nine@gmail.com>
2014-01-25 18:27:01 -0500

    Update views.py

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -88,8 +88,10 @@ def forgot(request):
                 user = User.objects.get(username=inp_username)
                 if inp_new_pw != inp_confirm_new_pw:
                     raise forms.ValidationError("Passwords do not match.")
-                if inp_email == user.email:
-                    send_mail('Reset Password', 'Your password has been reset.', settings.EMAIL_HOST_USER, [user.email])
+                else:
+                    if inp_email == user.email:
+                        user.set_password(inp_new_pw)
+                        send_mail('Reset Password', 'Your password has been reset.', settings.EMAIL_HOST_USER, [user.email])
                     return render(request, 'email_sent.html')
             except User.DoesNotExist:
                 pass 
