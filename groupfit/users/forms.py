7ea6ba04d988d4feee3dbe3135e8560c84c491f9
kish9nine <kish9nine@gmail.com>
2014-01-24 20:52:06 -0500

    Update forms.py

--- groupfit/users/forms.py
+++ groupfit/users/forms.py
@@ -5,43 +5,43 @@ from django.contrib.auth.models import User
 
 class RegisterForm(ModelForm):
 
+    username = forms.CharField(
+        required = True, 
+        #label="USERNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholer':'Username'})
+        )
+        
+    first_name = forms.CharField(
+        required = True, 
+        #label="FIRSTNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholer': 'Firstname'})
+        )
+        
+    last_name = forms.CharField(
+        required = True, 
+        #label="LASTNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholer':'Lastname'})
+        )
+        
+    password = forms.CharField(
+        required = True, 
+        #label="PASSWORD", 
+        widget=PasswordInput(attrs={'class':'form-control', 'placeholer':'Password'})
+        )
+        
+    """confirm_password = models.CharField(
+        required = True, 
+        label="CONFIRM_PASSWORD", 
+        PasswordInput(attrs={'class':'form-control', 'placeholer':'Confirm password'}))"""
+        
+    email = forms.EmailField(
+        required = True, 
+        #label="EMAIL", 
+        widget=EmailInput(attrs={'class':'form-control', 'placeholer':'Email'})
+        )
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'password', 'email']
         
-        #Required fields. Let's see if this works. 
-        required = ['username', 'first_name', 'last_name', 'password', 'email']
-        
-        
-        #Labels for each field.
-        label = {
-            'username': 'USERNAME',
-            'first_name': 'FIRSTNAME',
-            'last_name': 'LASTNAME',
-            'password': 'PASSWORD',
-            'confirm_password': 'CONFIRM PASSWORD',
-            'email': 'EMAIL',
-            }
-            
-        widgets = {
-            'username': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Username',
-            }),
-            'first_name': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'First Name',
-            }),
-            'last_name': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Last Name',
-            }),
-            'password': PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Password',
-            }),
-            'email': EmailInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Email',
-            }),
-        }
+       
