d8ada9a0ac34a1730a427c531e0eb1ee4a9079bf
kish9nine <kish9nine@gmail.com>
2014-01-24 19:22:00 -0500

    Update forms.py

--- groupfit/groupfit/forms.py
+++ groupfit/groupfit/forms.py
@@ -2,6 +2,6 @@ from django import forms
 from django.forms import TextInput, EmailInput
 
 class ForgotPasswordForm(forms.Form):
-    username = forms.CharField(max_length=20, widget = forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    username = forms.CharField(max_length=20, widget = forms.TextInput(attrs={'class': 'form-control', "placeholder": "Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', "placeholder": "Password"}))
     
