06da51f82cb26726366403283a3adba2354dec0e
kish9nine <kish9nine@gmail.com>
2014-01-25 11:28:24 -0500

    Update views.py

--- groupfit/groupfit/views.py
+++ groupfit/groupfit/views.py
@@ -91,7 +91,7 @@ def forgot(request):
                     password_reset(request, 
                         template_name='password_reset_form.html', 
                         email_template_name='password_reset_email.html')
-                    
+                    password_reset_done(request)
                     return render(request, 'email_sent.html')
             except User.DoesNotExist:
                 pass 
