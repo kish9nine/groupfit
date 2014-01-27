4e0507854005e695856b33aa9ae8695d6e4b39b3
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-27 17:37:54 -0500

    Registered Workout with admin

--- groupfit/groupfit/admin.py
+++ groupfit/groupfit/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from groupfit.models import WorkoutGoal
+from groupfit.models import WorkoutGoal, Workout
 
 # Register your models here.
 admin.site.register( WorkoutGoal )
+admin.site.register( Workout )
