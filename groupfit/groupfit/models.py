cacdd4d376b2109ed6700b2927a34a90a390c5d0
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-27 17:36:36 -0500

    Added workout model

--- groupfit/groupfit/models.py
+++ groupfit/groupfit/models.py
@@ -42,3 +42,63 @@ class WorkoutGoal( models.Model ):
 
     def __unicode__(self):
         return "%s" % self.name
+
+
+class Workout( models.Model ):
+    """
+    This model represents a single workout log for an individual. The
+    individual makes a workout log, then these are aggregated to indicate
+    "progress".
+    """
+
+    user = models.ForeignKey(
+        'users.UserProfile',
+        null = False,
+    )
+
+    """
+    These fields are mandatory; quantifiable.
+    """
+
+    date = models.DateField(
+        blank = False,
+    )
+
+    amount = models.CharField(
+        blank = False,
+        help_text = "How much did you workout?",
+        max_length = 50,
+    )
+
+    activity = models.CharField(
+        blank = False,
+        help_text = "What activity were you performing?",
+        max_length = 50,
+    )
+
+    units = models.CharField(
+        blank = False,
+        help_text = "What type of thing does this workout describe?",
+        max_length = 50,
+    )
+
+
+    """
+    These fields are optional; subjective.
+    """
+
+    description = models.CharField(
+        blank = True,
+        help_text = "Describe your workout",
+        max_length = 400,
+    )
+
+    energy_level = models.CharField(
+        blank = True,
+        help_text = "Describe your energy level during this workout.",
+        max_length = 50,
+    )
+
+
+    def __unicode__(self):
+        return "%s" % self.name
