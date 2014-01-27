2c248ed72c5b9d45627c73f845a155adbb77d624
kish9nine <kish9nine@gmail.com>
2014-01-27 02:53:50 -0500

    Update views.py

--- groupfit/users/views.py
+++ groupfit/users/views.py
@@ -23,8 +23,7 @@ def create_user(request):
 
             pw = create_user_form.cleaned_data.get('password')
             confirm_pw = confirm_password_form.cleaned_data.get('confirm_password')
-            new_user.save(commit=False)
-            email_overlap = User.objects.filter(email = confirm_password_form.cleaned_data.get('email')).exists() 
+            email_overlap = User.objects.filter(email = create_user_form.cleaned_data.get('email')).exists() 
             if pw == confirm_pw and not email_overlap:
                 new_user.set_password( pw )
                 new_user.save()
