36debbb99ea1aa7321b9a4f9c501a8a171f9e27e
kish9nine <kish9nine@gmail.com>
2014-01-27 18:57:23 -0500

    Update forms.py

--- groupfit/users/forms.py
+++ groupfit/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm, TextInput, PasswordInput, EmailInput
 from users.models import UserProfile
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 class PasswordForm(forms.Form):
     confirm_password = forms.CharField(
@@ -56,3 +57,36 @@ class RegisterForm(ModelForm):
             raise forms.ValidationError('Passwords do not match!')
         return pw2
     """
+
+
+#@login_required
+class EditUserProfileForm:
+    username = forms.CharField(
+        required = True, 
+        label="USERNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholder':'Username'})
+        )
+        
+    first_name = forms.CharField(
+        required = True, 
+        label="FIRSTNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholder': 'First Name'})
+        )
+        
+    last_name = forms.CharField(
+        required = True, 
+        label="LASTNAME", 
+        widget=TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'})
+        )
+        
+    password = forms.CharField(
+        required = True, 
+        label="PASSWORD", 
+        widget=PasswordInput(attrs={'class':'form-control', 'placeholder':'Reset password',})
+        )
+        
+    email = forms.EmailField(
+        required = True, 
+        label="EMAIL", 
+        widget=EmailInput(attrs={'class':'form-control', 'placeholder':'Email'})
+        )
