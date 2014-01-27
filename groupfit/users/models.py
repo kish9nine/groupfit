7f7fd99ae6fa916f99b945b1dd83dc075a837d09
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-27 02:50:49 -0500

    experimental

--- groupfit/users/models.py
+++ groupfit/users/models.py
@@ -60,4 +60,4 @@ def create_user_profile( sender, instance, created, **kwargs ):
     if created:
         UserProfile.objects.create(user=instance)
 #post_save.connect(create_user_profile, sender=User)
-#post_save.connect(create_user_profile, sender=FacebookBackend)
+post_save.connect(create_user_profile, sender=FacebookBackend)
