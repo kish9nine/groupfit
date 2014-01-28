11f5edec6dfd9f7032e2c6f3bacc567edcbe7f42
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-28 16:19:52 -0500

    Fixed user registration

--- groupfit/users/views.py
+++ groupfit/users/views.py
@@ -29,20 +29,14 @@ def create_user(request):
             email_overlap = User.objects.filter(email = create_user_form.cleaned_data.get('email')).exists() 
             if pw == confirm_pw and not email_overlap:
                 new_user.set_password( pw )
-                
 
                 # This should already automatically create a UserProfile.
+                new_user.save()
 
-                ##Then create UserProfile object from User object.
-                #new_UserProfile = UserProfile(user=new_user)
-                ##new_UserProfile.user = new_user
-                #new_UserProfile.save() #Then save. 
-                
                 # Set the new user's privacy setting accordingly.
                 privacy_setting = privacy_form.cleaned_data.get('privacy_setting')
-                request.user.userprofile.privacy = privacy_settting
+                new_user.userprofile.privacy = privacy_setting
                 
-                new_user.save()
                 #Send a welcome email. 
                 send_mail('Welcome to GroupFit!', 'Welcome to GroupFit!', settings.EMAIL_HOST_USER, [new_user.email])
  
@@ -53,8 +47,15 @@ def create_user(request):
     else:
         #If the user didn't plug in anything, create_user_form will be an empty shell?
         create_user_form = RegisterForm()
+        privacy_form = PrivacyForm()
         confirm_password_form = PasswordForm()
-    return render(request, 'register.html', {'create_user_form': create_user_form, "confirm_password_form": confirm_password_form})
+
+    return render(request, 'register.html', {
+        'create_user_form': create_user_form,
+        "confirm_password_form": confirm_password_form,
+        'privacy_form': privacy_form,
+    },
+    )
 
 
 
