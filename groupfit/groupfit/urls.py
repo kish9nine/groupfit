0bd8e90424b8c8730908f2e9cc8c7e31bcb9d3c7
kish9nine <kish9nine@gmail.com>
2014-01-28 00:26:45 -0500

    Update urls.py

--- groupfit/groupfit/urls.py
+++ groupfit/groupfit/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 from groupfit.views import landing_page, about, contact, terms
-from groupfit.views import privacy, forgot, community
+from groupfit.views import privacy, forgot, community, reset_password
 from django.contrib.auth.views import login, logout
 from users.views import view_user, create_user
 
